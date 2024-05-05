6e3a55b1d0856372abad981104c95bdf24d162cf
18thanhlongpham <18thanhlongpham@csu.fullerton.edu>
2024-05-05 00:49:58 -0700

    yes

--- src/pages/Employee Metrics.h3a1g.js
+++ src/pages/Employee Metrics.h3a1g.js
@@ -9,21 +9,42 @@ $w.onReady(function () {
             console.log("Value of employeeId:", id);
         }, 1000); 
 
-        // Add an onBeforeSave event handler to the dataset
-        $w("#dataset1").onBeforeSave(async () => {
-            return {
-                employeeId: id,  // Add the employeeId to the data
-            };
-        });
-
         $item("#button1").onClick(async () => {
-            // Clear the input fields
-            $item("#timePicker3").value = "";
-            $item("#timePicker4").value = "";
-            $item("#input1").value = "";
-            $item("#input2").value = "";
-            $item("#textBox1").value = "";
-            $item("#datePicker2").value = "";
+            // Get the input values
+            const expectedLogin = $item("#timePicker3").value;
+            const expectedLogout = $item("#timePicker4").value;
+            const expectedTasksCompleted = Number($item("#input1").value);
+            const expectedHoursWorked = Number($item("#input2").value);
+            const comments = $item("#textBox1").value;
+            const dateA1 = new Date($item("#datePicker2").value);
+
+            // Construct the data to be submitted
+            const dataToSubmit = {
+                employeeId: id, // Now id is accessible here
+                expectedLogin: expectedLogin,
+                expectedLogout: expectedLogout,
+                expectedTasksCompleted: expectedTasksCompleted,
+                expectedHoursWorked: expectedHoursWorked,
+                comments: comments,
+                dateA1: dateA1
+            };
+
+            try {
+                // Submit the data
+                await wixData.insert('Expected', dataToSubmit);
+                console.log('Form data saved successfully!');
+
+                // Clear the input fields
+                $item("#timePicker3").value = "";
+                $item("#timePicker4").value = "";
+                $item("#input1").value = "";
+                $item("#input2").value = "";
+                $item("#textBox1").value = "";
+                $item("#datePicker2").value = "";
+
+            } catch (error) {
+                console.error('Error saving form data:', error);
+            }
         });
     });
 });
