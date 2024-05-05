76bcb4f5562e34a5ab0617bcb3bba2f9f7027018
18thanhlongpham <18thanhlongpham@csu.fullerton.edu>
2024-05-04 17:32:08 -0700

    asd

--- src/pages/Dashboard.vslht.js
+++ src/pages/Dashboard.vslht.js
@@ -1,6 +1,6 @@
 import wixData from 'wix-data';
 
-$w.onReady(function () {
+$w.onReady(async function () {
     // Add a delay before collapsing the tables and hiding the progress bars
     setTimeout(() => {
         $w("#table5").collapse();
@@ -17,6 +17,9 @@ $w.onReady(function () {
         opts.shift(); 
         $w("#dropdown1").options = opts; 
     }, 2000); // Delay of 2 seconds
+
+    // Call the storeDivisionResult function
+    await storeDivisionResult();
 });
 
 $w("#dropdown1").onChange( (event) => {
@@ -45,23 +48,20 @@ async function storeDivisionResult() {
         let results1 = await wixData.query('EmployeeDatabase')
             .find();
         let number1 = results1.items[0]['hoursWorked']; 
-  
+
         // Query the second table
         let results2 = await wixData.query('Expected')
             .find();
         let number2 = results2.items[0]['expectedHoursWorked']; 
-  
+
         // Divide the numbers
         let divisionResult = number1 / number2;
-        console.log('Division result:', divisionResult);
-        console.log('Division result:', number1);
-        console.log('Division result:', number2);
-  
+
         // Store the result in a dataset
         let result = await wixData.insert('Calculate', { calc: divisionResult }); 
-  
+
         console.log('Division result stored successfully:', result);
     } catch (error) {
         console.error('Error storing division result:', error);
     }
-  }
+}
